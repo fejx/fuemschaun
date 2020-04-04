32d95cf0e4fdd8f3ddfd01f6ecec0d38c832133f
hoermannpaul <hoermannpaul@gmail.com>
2020-04-04 19:54:40 +0200

    add deployment script

---
+++ server/autodeploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [[ "$(git status -uno | grep up-to-date)" == "" ]]; then
+	echo "Updates available, pulling and deploying";
+	git pull;
+	make prod;
+else
+	echo "No changes, skip deploying";
+fi
