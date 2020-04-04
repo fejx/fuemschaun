8036e0d8bee0478db4a3bc2e934d108aa6526d13
fejx <fejx@users.noreply.github.com>
2020-04-04 16:22:45 +0200

    Search video element only if popup is triggered

--- client/popup.js
+++ client/popup.js
@@ -6,25 +6,25 @@ window.browser = (function () {
 
 const statusText = document.getElementById('status-text')
 
-// getActiveTab().then((tab) => {
-//     browser.tabs.executeScript(
-//         tab.id,
-//         { file: 'content-script.js' }
-//     )
-//     browser.runtime.onMessage.addListener((message, sender) => {
-//         if (message.name == 'video-element') {
-//             if (message.found)
-//                 statusText.innerHTML = 'Video found'
-//             else
-//                 statusText.innerHTML = 'No video found'
-//         }
-//     })
-// })
+getActiveTab().then((tab) => {
+    browser.tabs.executeScript(
+        tab.id,
+        { file: 'content-scripts/detect-video-element.js' }
+    )
+    browser.runtime.onMessage.addListener((message, sender) => {
+        if (message.name == 'video-element-found') {
+            if (message.found)
+                statusText.innerHTML = 'Video found'
+            else
+                statusText.innerHTML = 'No video found'
+        }
+    })
+})
 
-// function getActiveTab() {
-//     return new Promise((resolve, reject) => {
-//         browser.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-//             resolve(tabs[0])
-//         })
-//     })
-// }
+function getActiveTab() {
+    return new Promise((resolve, reject) => {
+        browser.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+            resolve(tabs[0])
+        })
+    })
+}
