fe31399afe406ab41ac549798adea926a8db1fd7
fejx <fejx@users.noreply.github.com>
2020-04-05 10:50:38 +0200

    Add video listener

---
+++ client/src/js/content/video-listener.js
@@ -0,0 +1,17 @@
+
+/**
+ * 
+ * @param {Node} videoElement Video node
+ * @param {playbackChangedCallback} onPlaybackChanged 
+ * @param {*} onPositionChanged 
+ * @param {*} onBuffering 
+ */
+export function addListeners(videoElement, onPlaybackChanged, onPositionChanged, onBuffering) {
+
+}
+
+/**
+ * Event callback when playback changed
+ * @callback playbackChangedCallback
+ * @param {boolean} playing True if the new state is playing
+ */
