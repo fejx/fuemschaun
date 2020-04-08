ca248b6421d133abe6f86e5204959f47e030d4d4
fejx <fejx@users.noreply.github.com>
2020-04-08 09:50:01 +0200

    In video wrapper, stop listening to events triggered by itself

--- client/src/js/content/video-wrapper.js
+++ client/src/js/content/video-wrapper.js
@@ -4,37 +4,52 @@ export class VideoWrapper {
     constructor(element) {
         this.element = element
         this.eventEmitter = new EventEmitter()
+        this.shouldSkipNextEvent = false
 
         this.element.addEventListener('pause', () => {
             console.info('pause event fired')
             const currentTime = this.element.currentTime
             if (this.isBuffering(this.element.buffered, currentTime)) {
                 console.info('is buffering')
-                this.eventEmitter.emit('buffering', { play: false, currentTime: currentTime })
+                this.emitOrSkip('buffering', { play: false, currentTime: currentTime })
             } else {
                 console.info('user paused')
-                this.eventEmitter.emit('playbackChanged', { play: false, currentTime: currentTime })
+                this.emitOrSkip('playbackChanged', { play: false, currentTime: currentTime })
             }
         })
         this.element.addEventListener('play', () => {
             console.info('play event fired')
-            this.eventEmitter.emit('playbackChanged', { play: true, currentTime: this.element.currentTime })
+            this.emitOrSkip('playbackChanged', { play: true, currentTime: this.element.currentTime })
         })
         this.element.addEventListener('seeked', () => {
             console.info('seeked event fired')
-            this.eventEmitter.emit('positionChanged', { play: true, currentTime: this.element.currentTime })
+            this.emitOrSkip('positionChanged', { play: true, currentTime: this.element.currentTime })
         })
     }
 
+    emitOrSkip(name, data) {
+        if (this.shouldSkipNextEvent)
+            this.shouldSkipNextEvent = false
+        else
+            this.eventEmitter.emit(name, data)
+    }
+
+    skipNextEvent() {
+        this.shouldSkipNextEvent = true
+    }
+
     play() {
+        this.skipNextEvent()
         this.element.play()
     }
 
     pause() {
+        this.skipNextEvent()
         this.element.pause()
     }
 
     jumpTo(position) {
+        this.skipNextEvent()
         this.element.currentTime = position
     }
 
