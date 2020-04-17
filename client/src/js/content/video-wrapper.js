0113209a5bda514872909b67bcfd49236e3d959a
fejx <fejx@users.noreply.github.com>
2020-04-17 16:41:27 +0200

    Simplify event listener creation in video-wrapper

--- client/src/js/content/video-wrapper.js
+++ client/src/js/content/video-wrapper.js
@@ -10,7 +10,7 @@ export class VideoWrapper {
         this.isPlaying = this.element.autoplay
 
         this.listeners = {
-            paused: () => {
+            pause: () => {
                 this.isPlaying = false
                 const currentTime = this.element.currentTime
                 if (this.isBuffering(this.element.buffered, currentTime)) {
@@ -21,7 +21,7 @@ export class VideoWrapper {
                     this.emitOrSkip('playbackChanged', { play: false, currentTime: currentTime })
                 }
             },
-            played: () => {
+            play: () => {
                 log.debug('Detected play')
                 this.isPlaying = true
                 this.emitOrSkip('playbackChanged', { play: true, currentTime: this.element.currentTime })
@@ -33,15 +33,17 @@ export class VideoWrapper {
             }
         }
 
-        this.element.addEventListener('pause', this.listeners.paused)
-        this.element.addEventListener('play', this.listeners.played)
-        this.element.addEventListener('seeked', this.listeners.seeked)
+        Object.keys(this.listeners).forEach(eventName => {
+            const listener = this.listeners[eventName]
+            this.element.addEventListener(eventName, listener)
+        })
     }
 
     removeListeners() {
-        this.element.removeEventListener('pause', this.listeners.paused)
-        this.element.removeEventListener('play', this.listeners.played)
-        this.element.removeEventListener('seeked', this.listeners.seeked)
+        Object.keys(this.listeners).forEach(eventName => {
+            const listener = this.listeners[eventName]
+            this.element.removeEventListener(eventName, listener)
+        })
     }
 
     emitOrSkip(name, data) {
