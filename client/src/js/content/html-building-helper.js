ef7e2b60561105f4ecbc67969677848e73140aa1
fejx <fejx@users.noreply.github.com>
2020-05-10 16:27:55 +0200

    Add prependNewElement

--- client/src/js/content/html-building-helper.js
+++ client/src/js/content/html-building-helper.js
@@ -4,9 +4,20 @@
  * @param {customizerCallback} customizer 
  */
 export function appendNewElement(container, tag, customizer) {
-    const newElement = document.createElement(tag)
-    customizer(newElement)
+    const newElement = createWithCustomizer(tag, customizer)
     container.appendChild(newElement)
+    return newElement
+}
+
+/**
+ * @param {HTMLDivElement} container 
+ * @param {string} tag 
+ * @param {customizerCallback} customizer 
+ */
+export function prependNewElement(container, tag, customizer) {
+    const newElement = createWithCustomizer(tag, customizer)
+    // This works even if there is no child
+    container.insertBefore(newElement, container.firstChild)
     return newElement
 }
 
@@ -15,3 +26,9 @@ export function appendNewElement(container, tag, customizer) {
  * @callback customizerCallback
  * @param {HTMLElement} element
  */
+
+function createWithCustomizer(tag, customizer) {
+    const newElement = document.createElement(tag)
+    customizer(newElement)
+    return newElement
+}
